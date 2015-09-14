d2549ac72be43cc83c03543567c424316c92eacf
Henric Andersson <ha@sensenet.nu>
2015-09-14 10:17:56 -0700

    Initial version

---
+++ compile.py
@@ -0,0 +1,133 @@
+#!/usr/bin/env python
+
+import re
+import sys
+import os
+import magic
+import base64
+
+base = ""
+tmp = []
+
+def readfile(filename):
+    try:
+        f = open(filename, 'r')
+        content = f.read()
+        f.close()
+        return content
+    except:
+        print "ERR: Couldn't find " + filename
+        return None
+
+def img_compile(filename):
+    try:
+        mime = magic.from_file(filename, mime=True)
+        data = base64.b64encode(readfile(filename))
+        return "data:%s;base64,%s" % (mime, data)
+    except Exception as inst:
+        print "ERR: %s (%s)" % (inst, type(inst))
+        return None
+
+def html_css_image(tag):
+    if len(tmp) > 0:
+        path = tmp[0]
+    else:
+        path = base
+
+    src = tag.group(1)
+    print "Substing: " + src
+    if not "://" in src:
+        src = img_compile(path + src)
+
+    if src is None:
+        print "Default"
+        src = tag.group(1)
+
+    return 'url("%s")' % src
+
+def html_process_css(data, path):
+    tmp.append(path)
+    result = re.sub(r'url\("([^"]+)"\)', html_css_image, data)
+    del tmp[:]
+    return result
+
+
+def html_embed_css(tag):
+    # Obtain the filename
+    p = re.compile('href="([^["\?]]+)"')
+    m = p.search(tag)
+    if m and not "://" in m.group(1):
+        content = readfile(base + m.group(1))
+        if content:
+            tmp = os.path.dirname(base + m.group(1)) + "/"
+            content = html_process_css(content, tmp)
+            return '<style type="text/css">' + content + '</style>'
+        else:
+            return tag
+#    else:
+#        print "No match for " + tag
+    return tag
+
+def html_embed_script(tag):
+    # Obtain the filename
+    p = re.compile('src="([^"]+)"')
+    m = p.search(tag)
+    if m and not "://" in m.group(1):
+        content = readfile(base + m.group(1))
+        if content:
+            return '<script type="text/javascript">' + content
+        else:
+            return tag
+#    else:
+#        print "No match for " + tag
+    return tag
+
+def html_img_src(m):
+    src = m.group(1)
+    if not "://" in src:
+        src = img_compile(base + src)
+
+    if src is None:
+        src = m.group(1)
+
+    return 'src="' + src + '"'
+
+def html_translate(m):
+    tag = m.group()
+    # Skip end-tags
+    if tag[1] == '/':
+        return m.group()
+
+    # See if any of these contains a src="" construct...
+    if tag[1:4] == "img":
+        tag = re.sub(r'src="([^"]+)"', html_img_src, tag)
+    elif tag[1:5] == "link":
+        tag = html_embed_css(tag)
+    elif tag[1:7] == "script":
+        tag = html_embed_script(tag)
+    return tag
+
+def html_compile(filename):
+    content = readfile(filename)
+    content = re.sub(r'<[^>]+>', html_translate, content)
+    return content
+
+
+if len(sys.argv) is not 3:
+    print "Usage: compile.py <input html> <output html>"
+    sys.exit(1)
+
+infile = sys.argv[1]
+outfile = sys.argv[2]
+
+print "Parsing %s into %s" % (infile, outfile)
+
+base = os.path.dirname(infile)
+if base is "":
+    base = "."
+base += "/"
+data = html_compile(infile)
+
+o = open(outfile, 'w')
+o.write(data)
+o.close()
